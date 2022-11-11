5ea899d862dfd453b7e8bb0ffad74256de7b9f9f
Dimash <d.esengeldiev@mail.ru>
2022-11-12 03:37:40 +0600

    props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React from "react";
+import "./scss/app.scss";
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Американская" price={700} />
+            <PizzaBlock title="Американская" price={700} />
+            <PizzaBlock title="Американская" price={700} />
+            <PizzaBlock title="Американская" price={700} />
+            <PizzaBlock title="Американская" price={700} />
+            <PizzaBlock title="Американская" price={700} />
+            <PizzaBlock title="Американская" price={700} />
+            <PizzaBlock title="Американская" price={700} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
