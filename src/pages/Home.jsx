b9fb1940c49f43bff10e9fc32fc1d5b34961744b
Dimash <d.esengeldiev@mail.ru>
2022-11-20 20:02:01 +0600

    нормализовал верстку

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,9 +17,10 @@ const Home = () => {
         setItems(json);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -39,7 +40,7 @@ const Home = () => {
               />
             ))}
       </div>
-    </>
+    </div>
   );
 };
 
